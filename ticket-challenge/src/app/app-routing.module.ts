01b8447649e132e9d33bc6a23b727487834c8853
linz <phoneticem@gmail.com>
2023-12-07 17:41:49 +0330

    salon lists - plan routes
    implemented 2 page routes(salon list, plan) and redirect any other routes(404 pages) to salon lists page, also added route param to plan page

--- ticket-challenge/src/app/app-routing.module.ts
+++ ticket-challenge/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SalonsListComponent} from "./salons-list/salons-list.component";
+import {PlanComponent} from "./plan/plan.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SalonsListComponent
+  },
+  {
+    path: 'plan/:id',
+    component: PlanComponent
+  },
+  {
+    path: '**', redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ticket-challenge/src/app/plan/plan.component.ts
+++ ticket-challenge/src/app/plan/plan.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
   styleUrls: ['./plan.component.scss']
 })
-export class PlanComponent implements OnInit {
-
-  constructor() { }
+export class PlanComponent implements OnInit, OnDestroy {
+  private routeSubscription: Subscription = new Subscription;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // You should get the plan array from the: base-url/map/<map_id>
+    this.routeSubscription = this.route.params.subscribe(params => {
+      console.log(params['id'])
+    });
   }
-
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
+  }
 }
